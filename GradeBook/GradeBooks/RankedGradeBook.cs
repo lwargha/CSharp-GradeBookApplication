9811d5eaa8f092544baca461227b5b04200a7bcb
Lucas Wargha <war18001@byui.edu>
2019-06-07 15:25:15 -0600

    Test

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -27,5 +27,22 @@ namespace GradeBook.GradeBooks
             else if (grades[(threshold * 4) - 1] <= averageGrade) return 'D';
             else return 'F';
         }
+          public override void CalculateStatistics() { 
+              if (Students.Count < 5) { 
+                  System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                  return;
+              } else { 
+                  base.CalculateStatistics();
+              }
+          }
+
+          public override void CalculateStudentStatistics(string name) { 
+              if (Students.Count < 5) { 
+                  System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                  return;
+              } else { 
+                  base.CalculateStudentStatistics(name);
+              }
+          }
     }
 }
